7cc6d52c0fa0afcb68cc8347305e66f30ae35769
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2024-05-27 16:40:08 +0200

    added tabalonia to avalonia

---
+++ OpalTest/ViewModels/TabItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace OpalTest.ViewModels
+{
+    public class TabItemViewModel : ObservableObject
+    {
+        public string Header { get; set; }
+        
+        public string SimpleContent { get; set; }
+
+
+        public override string ToString() => Header;
+    }
+}
