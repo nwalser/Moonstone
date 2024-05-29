95997286bfd9f8cda63faab6130ef640e998b2fb
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2024-05-29 16:22:43 +0200

    added app menu

--- Sapphire.Electron/tailwind.config.js
+++ Sapphire.Electron/tailwind.config.js
@@ -1,8 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{razor,html,cshtml}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      background: colors.neutral,
+      primary: colors.slate,
+    },
   },
   plugins: [],
 }
