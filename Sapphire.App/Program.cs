8ba04fb4698eb7634b8356d1204d85376b10166c
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2024-06-01 18:52:15 +0200

    added new project

--- Sapphire.App/Program.cs
+++ Sapphire.App/Program.cs
@@ -1,38 +1,39 @@
 using ElectronNET.API;
 using ElectronNET.API.Entities;
 using Microsoft.FluentUI.AspNetCore.Components;
+using Sapphire.App.Services;
 using Sapphire.Data;
-using Sapphire.Electron.Services;
-using Toolbelt.Blazor.Extensions.DependencyInjection;
+using App = Sapphire.App.Components.App;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseElectron(args);
 builder.Services.AddElectron();
 
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddHotKeys2();
-
-builder.Services.AddHttpClient();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 builder.Services.AddFluentUIComponents();
 
 builder.Services.AddSingleton(new DatabaseManager<ProjectDatabase>());
 
+
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+app.UseHttpsRedirection();
+
 app.UseStaticFiles();
+app.UseAntiforgery();
 
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 await app.StartAsync();
 
---
+++ Sapphire.App/Services/DatabaseManager.cs
@@ -0,0 +1,48 @@
+﻿using DeviceId;
+using Moonstone.Database;
+
+namespace Sapphire.App.Services;
+
+public class DatabaseManager<TType> where TType : Database, new()
+{
+    public TType? Database { get; private set; }
+
+
+    public void Create(string path)
+    {
+        if (Database is not null) throw new InvalidOperationException();
+        
+        var deviceId = new DeviceIdBuilder()
+            .AddMachineName()
+            .AddMacAddress()
+            .AddUserName()
+            .ToString();
+        var db = new TType();
+        db.Create(path, deviceId);
+
+        Database = db;
+    }
+
+    public void Open(string path)
+    {
+        if (Database is not null) throw new InvalidOperationException();
+
+        var deviceId = new DeviceIdBuilder()
+            .AddMachineName()
+            .AddMacAddress()
+            .AddUserName()
+            .ToString();
+        var db = new TType();
+        db.Open(path, deviceId);
+
+        Database = db;
+    }
+
+    public void Close(string path)
+    {
+        if (Database is null) throw new InvalidOperationException();
+
+        Database.Close();
+        Database = null;
+    }
+}
