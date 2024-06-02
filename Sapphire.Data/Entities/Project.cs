7056d7ce77771134529ea351ba8fe3df8e63d5f1
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2024-06-02 14:12:02 +0200

    extended theme

--- Sapphire.Data/Entities/Project.cs
+++ Sapphire.Data/Entities/Project.cs
@@ -5,6 +5,7 @@ namespace Sapphire.Data.Entities;
 public class Project : Document
 {
     public required string Name { get; set; }
+    public string Description { get; set; } = string.Empty;
     
     public DateOnly? Deadline { get; set; }
 }
