443cde2b7b6d1a37410ccc034cf3bd68897bd0c7
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2024-06-07 21:37:59 +0200

    added leave day view

--- Sapphire.Data/Entities/WorkingHours/LeaveDayAggregate.cs
+++ Sapphire.Data/Entities/WorkingHours/LeaveDayAggregate.cs
@@ -6,4 +6,26 @@ public class LeaveDayAggregate : Document
 {
     public Guid WorkerId { get; set; }
     public DateOnly Date { get; set; }
+
+    
+    public static List<LeaveDayAggregate> FromRange(Guid workerId, DateOnly start, DateOnly end)
+    {
+        return Enumerable.Range(0, end.DayNumber - start.DayNumber + 1)
+            .Select(start.AddDays)
+            .Select(d => new LeaveDayAggregate()
+            {
+                WorkerId = workerId,
+                Date = d
+            })
+            .ToList();
+    }
+    
+    public static IEnumerable<IGrouping<DateOnly, LeaveDayAggregate>> GetGrouped(IEnumerable<LeaveDayAggregate> leaveDays)
+    {
+        return leaveDays
+            .DistinctBy(d => d.Date)
+            .OrderBy(d => d.Date)
+            .Select((d, i) => new { date = d, key = d.Date.AddDays(-i) })
+            .GroupBy(d => d.key, tuple => tuple.date);
+    }
 }
