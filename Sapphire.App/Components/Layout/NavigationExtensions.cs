736f8266f195a8a40b8d357a404d85a5f6bfc65b
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2024-06-09 19:36:04 +0200

    implemented current page highlighting

---
+++ Sapphire.App/Components/Layout/NavigationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Sapphire.App.Components.Layout;
+
+public static class NavigationExtensions
+{
+    public static string GetSelectedStyle(this NavigationManager navigation, string pattern)
+    {
+        return navigation.ToBaseRelativePath(navigation.Uri).StartsWith(pattern[1..]) 
+            ? "background-color: var(--mud-palette-lines-default) !important" : string.Empty;
+    }
+}
