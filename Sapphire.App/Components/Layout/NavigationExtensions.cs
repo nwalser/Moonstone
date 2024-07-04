79db6dffa2fa8b0f0b2ad6e27c5b011180108f69
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2024-07-04 14:28:46 +0200

    added navigation extensions

--- Sapphire.App/Components/Layout/NavigationExtensions.cs
+++ Sapphire.App/Components/Layout/NavigationExtensions.cs
@@ -6,7 +6,11 @@ public static class NavigationExtensions
 {
     public static string GetSelectedStyle(this NavigationManager navigation, string pattern)
     {
-        return navigation.ToBaseRelativePath(navigation.Uri).StartsWith(pattern[1..]) 
-            ? "background-color: var(--mud-palette-lines-default) !important" : string.Empty;
+        return IsSelected(navigation, pattern) ? "background-color: var(--mud-palette-lines-default) !important" : string.Empty;
+    }
+
+    public static bool IsSelected(this NavigationManager navigation, string pattern)
+    {
+        return navigation.ToBaseRelativePath(navigation.Uri).StartsWith(pattern[1..]);
     }
 }
